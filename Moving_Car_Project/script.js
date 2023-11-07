800dfdf50af94e420d35fdad3e982167c9ab6bff
killlercram <herosviral@gmail.com>
2023-11-07 18:56:25 +0530

    This is a simple project where the car feels to be moving.It is not at all responsive but made with all three components.

---
+++ Moving_Car_Project/script.js
@@ -0,0 +1,8 @@
+// var audio=document.createElement('audio');
+// audio.setAttribute('src','Sounds/sound.mp3');
+// audio.loop=true;
+// audio.play();
+
+var audio=new Audio('Sounds/sound.mp3');
+audio.loop=true;
+audio.play();
